adfb39740814e7cf7168ae8f0de3a22ef1e502ed
Bianca Bojescu <bianca.bojescu@gmail.com>
2024-04-24 15:47:04 +0300

    login

--- licenta2/app.py
+++ licenta2/app.py
@@ -14,5 +14,10 @@ def login():
     return render_template('login.html')
 
 
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
 if __name__ == '__main__':
     app.run()
