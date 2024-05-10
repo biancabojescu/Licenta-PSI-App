7d96b72dfdf9bcf4bb0fb2dc78b9943c6946fb91
Bianca Bojescu <bianca.bojescu@gmail.com>
2024-05-10 15:40:26 +0300

    db

--- licenta2/app.py
+++ licenta2/app.py
@@ -1,6 +1,22 @@
-from flask import Flask, render_template
+import os
+import psycopg2
+from dotenv import load_dotenv
+from flask import g, Flask, render_template
 
+load_dotenv()
 app = Flask(__name__)
+
+
+@app.before_request
+def before_request():
+    g.db = psycopg2.connect(os.getenv("DATABASE_URL"))
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
 
 
 @app.route('/')
