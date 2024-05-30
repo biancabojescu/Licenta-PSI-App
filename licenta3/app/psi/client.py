3cbe8a8fa491eae97a5e010aab81c6283d34222f
Bianca Bojescu <bianca.bojescu@gmail.com>
2024-05-30 14:30:50 +0300

    prima incercare de implementare

---
+++ licenta3/app/psi/client.py
@@ -0,0 +1,58 @@
+import hashlib
+import secrets
+from Crypto.Util.number import inverse
+
+from app.psi.server import decrypt
+
+
+def hash_function(y):
+    return int(hashlib.sha256(y.encode()).hexdigest(), 16)
+
+
+def generate_random_elements(NC):
+    return secrets.randbelow(NC)
+
+
+def encrypt(key, element):
+    e, n = key.e, key.n
+    return pow(element, e, n)
+
+
+def generate_random_numbers(pb_key, NC_max=1024):
+    rand_nums = []
+
+    for _ in range(NC_max):
+        r = generate_random_elements(pb_key.n)
+        r_inv = inverse(r, pb_key.n)
+        r_encrypt = encrypt(pb_key, r)
+        rand_nums.append((r_inv, r_encrypt))
+
+    return rand_nums
+
+
+def blind_batch(Y, rand_nums, n):
+    A = []
+    for y, rf in zip(Y, rand_nums):
+        r_encrypt = rf[1]
+        A.append((r_encrypt * y) % n)
+    return A
+
+
+def verify_batch(B, rand_nums, n):
+    S = []
+    for b, rf in zip(B, rand_nums):
+        r_inv = rf[0]
+        S.append((b * r_inv) % n)
+    return S
+
+
+def generate_update_positions(n, NU_max=100):
+    update_positions = []
+    for _ in range(NU_max):
+        update_positions.append(secrets.randbelow(n))
+    return update_positions
+
+
+def modify_bloom_filter(bf, P, pv_key, U):
+    for i in P:
+        bf.add(decrypt(pv_key, U[i]))
---
+++ licenta3/app/psi/server.py
@@ -0,0 +1,38 @@
+import pybloom_live
+from Crypto.PublicKey import RSA
+
+
+def gen_pv_rsa_key(n, exp):
+    return RSA.generate(n, e=exp)
+
+
+def decrypt(key, element):
+    d, n = key.d, key.n
+    return pow(element, d, n)
+
+
+def bloom_filter(pv_key, X):
+    mode = pybloom_live.ScalableBloomFilter.SMALL_SET_GROWTH
+    bf = pybloom_live.ScalableBloomFilter(mode=mode)
+
+    for x in X:
+        bf.add(decrypt(pv_key, x))
+
+    return bf
+
+
+def sign_batch(pv_key, A):
+    B = []
+    for a in A:
+        B.append(decrypt(pv_key, a))
+    return B
+
+
+def update_bloom_filter(bf, pv_key, P, X):
+    for i in P:
+        bf.add(decrypt(pv_key, X[i]))
+
+
+def insert_new_elements(bf, pv_key, U):
+    for u in U:
+        bf.add(decrypt(pv_key, u))
