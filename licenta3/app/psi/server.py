66f6c90f67da2f4af175e7b9774f3bc175e94d62
Bianca Bojescu <bianca.bojescu@gmail.com>
2024-06-10 20:25:57 +0300

    bloom filter

--- licenta3/app/psi/server.py
+++ licenta3/app/psi/server.py
@@ -1,4 +1,25 @@
-import pybloom_live
+import hashlib
+from bitarray import bitarray
+
+
+class BloomFilter:
+    def __init__(self, size, hash_count):
+        self.size = size
+        self.hash_count = hash_count
+        self.bit_array = bitarray(size)
+        self.bit_array.setall(0)
+
+    def _hashes(self, item):
+        item_str = str(item)
+        return [int(hashlib.md5((item_str + str(i)).encode('utf-8')).hexdigest(), 16) % self.size for i in
+                range(self.hash_count)]
+
+    def add(self, item):
+        for hash_value in self._hashes(item):
+            self.bit_array[hash_value] = True
+
+    def __contains__(self, item):
+        return all(self.bit_array[hash_value] for hash_value in self._hashes(item))
 
 
 def decrypt(key, element):
@@ -6,12 +27,12 @@ def decrypt(key, element):
     return pow(element, d, n)
 
 
-def bloom_filter(pv_key, X):
-    mode = pybloom_live.ScalableBloomFilter.SMALL_SET_GROWTH
-    bf = pybloom_live.ScalableBloomFilter(mode=mode)
+def bloom_filter(pv_key, X, size=1000, hash_count=10):
+    bf = BloomFilter(size, hash_count)
 
     for x in X:
-        bf.add(decrypt(pv_key, x))
+        decrypted_element = str(decrypt(pv_key, x))
+        bf.add(decrypted_element)
 
     return bf
 
@@ -25,9 +46,11 @@ def semneaza_datele(pv_key, A):
 
 def update_bf(bf, pv_key, P, X):
     for i in P:
-        bf.add(decrypt(pv_key, X[i]))
+        decrypted_element = str(decrypt(pv_key, X[i]))
+        bf.add(decrypted_element)
 
 
 def insert_elements_bf(bf, pv_key, U):
     for u in U:
-        bf.add(decrypt(pv_key, u))
+        decrypted_element = str(decrypt(pv_key, u))
+        bf.add(decrypted_element)
